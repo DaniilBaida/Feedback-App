b6781a7aed6d59ba29d4a91f548158459d295d22
Daniil Baida <daniil.baida.0118924@edu.atec.pt>
2022-04-02 14:45:59 +0100

    Feedback App Project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from "react"
 import ReactDOM from 'react-dom'
+import App from './App'
+import './styles.css'
 
-ReactDOM.render(<h1>MY APP</h1>, document.getElementById('root'))
+ReactDOM.render(
+    <React.StrictMode>
+    <App />
+    </React.StrictMode>
+    , document.getElementById('root'))
